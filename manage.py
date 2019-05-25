62d18ae8b2e7bba56bac9c269bb98c83790d3f16
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-05-25 11:47:25 +0800

    静态文件服务

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ohmyrss.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- rssfeeds/settings.py
+++ rssfeeds/settings.py
@@ -15,7 +15,7 @@ SPIDER_MODULES = ['rssfeeds.spiders']
 NEWSPIDER_MODULE = 'rssfeeds.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# Crawl responsibly by identifying yourself (and your ohmyrss) on the user-agent
 #USER_AGENT = 'rssfeeds (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
@@ -24,7 +24,7 @@ ROBOTSTXT_OBEY = True
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
-# Configure a delay for requests for the same website (default: 0)
+# Configure a delay for requests for the same ohmyrss (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
