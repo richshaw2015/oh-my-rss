abdd449a763f3b26aee4da5624aa5105886094f3
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-05-28 09:53:11 +0800

    调整样式

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     // 页面样式
-    height = ($(window).height() - $('.page-footer').height() - $('header').height())  + 'px';
-    $('.cnt-detail').css({'height': height, 'maxHeight': height})
+    const height = ($(window).height() - $('.page-footer').height() - $('header').height())  + 'px';
+    $('.cnt-detail').css({'height': height, 'maxHeight': height});
+    $('.cnt-index').css({'height': height, 'maxHeight': height});
 });
