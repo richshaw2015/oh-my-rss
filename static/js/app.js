1c16c0e61ec4c3be5eb5efd4fe2cebc74e3e6a91
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-05-30 10:11:53 +0800

    调通手风琴效果

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
+jQuery(document).ready(function () {
+    M.AutoInit();
+
+    //Init Collapsible
+    jQuery('.collapsible').collapsible();
+
+    var elem = document.querySelector('.collapsible');
+    var instance = new M.Collapsible(elem, {
+        // inDuration: 1000,
+        // outDuration: 1000
+    });
 
     // 页面样式
-    const height = ($(window).height() - $('.page-footer').height() - $('header').height())  + 'px';
+    const height = ($(window).height() - $('.page-footer').height() - $('header').height()) + 'px';
     $('.cnt-detail').css({'height': height, 'maxHeight': height});
     $('.cnt-index').css({'height': height, 'maxHeight': height});
 });
