add73eb1732a7599db8f82344e45c1202205aa5d
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-06-08 16:30:21 +0800

    留言页面

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
 
-    //Init Collapsible
-    $('.collapsible').collapsible();
-    // Init Floating Bar
+    // 初始化组件
     $('.fixed-action-btn').floatingActionButton();
+    $('.tooltipped').tooltip();
+    $('.materialboxed').materialbox();
+    $('.modal').modal({"dismissible": false});
+    $('#modal-qrcode').modal({"dismissible": true});
+    $('textarea#issue-input-detail, input#issue-input-name, input#issue-input-contact').characterCounter();
 
     // 页面样式
     const cntHeight = ($(window).height() - $('.page-footer').height() - $('header').height()) + 'px';
