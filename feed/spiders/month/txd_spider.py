77c1ec2a0c98b63d0ee598e5baec534b8a939a5d
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-08-15 22:51:39 +0800

    spider repeat

--- feed/spiders/month/txd_spider.py
+++ feed/spiders/month/txd_spider.py
@@ -2,7 +2,7 @@
 from feed.spiders.spider import Spider
 
 
-class TxdSpider(Spider):
+class TxdWeeklySpider(Spider):
     name = 'txd'
 
     def __init__(self):
@@ -10,10 +10,9 @@ class TxdSpider(Spider):
                         start_urls=[
                             'https://www.yuque.com/txd-team/fe-report',
                         ],
-                        index_xpath="//a[contains(text(),'第 ')]/@href",
-                        article_title_xpath='//article//h1/text()',
-                        article_content_xpath='//article',
-                        article_trim_xpaths=[
-                            '//h1'
-                        ]
+                        index_xpath='//div[@class="main-book-cover-contents"]//span[@class="name"]//a/@href',
+                        article_title_xpath="//h1/text()",
+                        article_content_xpath="//div[@class='yuque-doc-content']",
+                        index_limit_count=2,
+                        browser=True
                         )
