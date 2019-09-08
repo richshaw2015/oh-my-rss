2beeee45efd1d1574f8c6f8d5ec00f7d936f304c
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-08 23:23:06 +0800

    add sf spider support

--- feed/spiders/spider.py
+++ feed/spiders/spider.py
@@ -51,3 +51,55 @@ class Spider(scrapy.Spider):
             req_url = url
         yield FeedItem(title=title, content=content, url=url, name=self.name, trims=self.article_trim_xpaths,
                        req_url=req_url)
+
+
+class TitleSpider(Spider):
+    """
+    article title is in the first page and content in the second page
+    """
+
+    def __init__(self, start_urls, index_xpath, article_title_xpath, article_content_xpath, article_trim_xpaths=None,
+                 index_limit_count=None, index_reverse=False, browser=False):
+        super(TitleSpider, self).__init__(start_urls, index_xpath, article_title_xpath, article_content_xpath,
+                                          article_trim_xpaths, index_limit_count, index_reverse, browser)
+
+    def parse(self, response):
+        content_urls = response.xpath(self.index_xpath).extract()
+        titles = response.xpath(self.article_title_xpath).extract()
+
+        if len(content_urls) != len(titles):
+            self.logger.warning(f'Xpath maybe wrong {self.index_xpath}`{self.article_title_xpath}')
+
+        if self.index_limit_count:
+            content_urls = content_urls[:self.index_limit_count]
+            titles = titles[:self.index_limit_count]
+
+        if content_urls:
+            for i in range(0, len(content_urls)):
+                content_url = content_urls[i]
+                title = titles[i]
+                full_url = urllib.parse.urljoin(self.start_urls[0], content_url)
+
+                if not is_crawled_url(full_url):
+                    self.logger.info(f"Begin crawl`{full_url}")
+                    yield response.follow(content_url, self.content_parse, meta={"title": title})
+        else:
+            self.logger.error("Site maybe has changed`%s", self.name)
+
+    def content_parse(self, response):
+        title = response.meta['title']
+
+        try:
+            content = response.xpath(self.article_content_xpath).extract_first()
+        except AttributeError:
+            self.logger.warning("Xpath Error`%s", response.url)
+            return False
+
+        url = response.url
+        try:
+            req_url = response.meta['redirect_urls'][0]
+        except KeyError:
+            req_url = url
+
+        yield FeedItem(title=title, content=content, url=url, name=self.name, trims=self.article_trim_xpaths,
+                       req_url=req_url)
---
+++ feed/spiders/week/segmentfault_spider.py
@@ -0,0 +1,21 @@
+
+from feed.spiders.spider import TitleSpider
+
+
+class SegmentfaultSpider(TitleSpider):
+    name = 'segmentfault'
+
+    def __init__(self):
+        TitleSpider.__init__(self,
+                        start_urls=[
+                            'https://segmentfault.com/weekly',
+                        ],
+                        index_xpath='//ul[@class="dirlist"]/li/a/@href',
+                        article_title_xpath='//ul[@class="dirlist"]/li/a/text()',
+                        article_content_xpath='//div[@class="edm__main"]',
+                        article_trim_xpaths=[
+                            '//div[@class="edm__footer"]',
+                            '//div[@class="edm__heading"]',
+                        ],
+                        index_limit_count=1,
+                        )
