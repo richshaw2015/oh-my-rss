73ab9d03656fb64089b0320a7c2c7100304f2514
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-10 20:48:28 +0800

    add PR from @bzd111

--- feed/spiders/week/golangweekly_spider.py
+++ feed/spiders/week/golangweekly_spider.py
@@ -1,15 +1,19 @@
 from feed.spiders.spider import Spider
 
 
-class RyfWeeklySpider(Spider):
+class GolangWeeklySpider(Spider):
     name = 'golangweekly'
 
     def __init__(self):
-        Spider.__init__(
-            self,
-            start_urls=['https://github.com/polaris1119/golangweekly'],
-            index_xpath="//article//a[contains(text(),'第')]/@href",
-            article_title_xpath='//article//h1/text()',
-            article_content_xpath="//article",
-            index_limit_count=3,
-        )
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://github.com/polaris1119/golangweekly'
+                        ],
+                        index_xpath="//article//a[contains(text(),'第')]/@href",
+                        article_title_xpath='//article//h1/text()',
+                        article_content_xpath="//article",
+                        index_limit_count=1,
+                        article_trim_xpaths=[
+                            "//article//h1",
+                        ]
+                        )
