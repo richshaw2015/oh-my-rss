f6218c773ae35d037f6c790acafd517517bbcc27
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-22 17:34:12 +0800

    add two sites

---
+++ feed/spiders/day/uxc_spider.py
@@ -0,0 +1,17 @@
+
+from feed.spiders.spider import Spider
+
+
+class UxcSpider(Spider):
+    name = 'uxc'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'http://ued.baidu.com/case',
+                        ],
+                        index_xpath="//ul[@class='case-article-list']/li/a/@href",
+                        article_title_xpath="//div[@class='article-title']/text()",
+                        article_content_xpath="//div[@class='article-content']",
+                        index_limit_count=1,
+                        )
