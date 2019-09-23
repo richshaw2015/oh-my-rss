11e78bc05faf68a2f2d817b7e579bbd1cc6617cb
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-23 22:28:35 +0800

    add site zcfy

---
+++ feed/spiders/day/zcfy_spider.py
@@ -0,0 +1,21 @@
+
+from feed.spiders.spider import Spider
+
+
+class ZcfySpider(Spider):
+    name = 'zcfy'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://zcfy.cc',
+                        ],
+                        index_xpath='//div[@id="main_list"]//div[@class="uk-width-expand"]/a/@href',
+                        article_title_xpath='//h2[@class="detail-title uk-margin-medium-top"]/text()',
+                        article_content_xpath='//div[@class="uk-card-body uk-padding-remove-top"]',
+                        index_limit_count=1,
+                        article_trim_xpaths=[
+                            '//div[@class="uk-padding uk-padding-remove-horizontal"]',
+                            '//h2[@class="detail-title uk-margin-medium-top"]',
+                        ]
+                        )
