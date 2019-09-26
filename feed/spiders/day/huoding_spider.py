f5dca5d0a65f843da9e5ec9e14be5e3fa54fc9e7
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-26 20:59:53 +0800

    add a site

---
+++ feed/spiders/day/huoding_spider.py
@@ -0,0 +1,17 @@
+
+from feed.spiders.spider import Spider
+
+
+class HuodingSpider(Spider):
+    name = 'huoding'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://blog.huoding.com/',
+                        ],
+                        index_xpath="//header/h1/a/@href",
+                        article_title_xpath="//article/header/h1/text()",
+                        article_content_xpath="//article//div[@class='entry-content']",
+                        index_limit_count=1,
+                        )
