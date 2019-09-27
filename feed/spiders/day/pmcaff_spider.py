25b73fb60fce8fae8fa4c30f699c5cf439549edb
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-09-27 12:42:11 +0800

    add a site pmcaff

---
+++ feed/spiders/day/pmcaff_spider.py
@@ -0,0 +1,18 @@
+
+from feed.spiders.spider import Spider
+
+
+class PmcaffSpider(Spider):
+    name = 'pmcaff'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://coffee.pmcaff.com/?type=2',
+                        ],
+                        index_xpath="//h2[@class='title']/a/@href",
+                        article_title_xpath="//div[@class='head']//h2[@class='title']/text()",
+                        article_content_xpath="//div[@id='articleContent']",
+                        index_limit_count=1,
+                        browser=True,
+                        )
