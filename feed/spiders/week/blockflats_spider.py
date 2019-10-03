9a42ea9ff70d4a60f0406c7abdbf28933880ef61
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-10-03 11:10:25 +0800

    site changed

--- feed/spiders/week/blockflats_spider.py
+++ feed/spiders/week/blockflats_spider.py
@@ -11,11 +11,12 @@ class BlockflatsSpider(Spider):
                             'https://blockflats.com/weekly',
                         ],
                         index_xpath="//div[@class='ignore-weeklys']/a/@href",
-                        article_title_xpath="//div[@class='ignore-container']/h3/text()",
+                        article_title_xpath="//div[@class='ignore-container']//div[@class='ignore-title']//text()",
                         article_content_xpath="//div[@class='ignore-container']",
                         article_trim_xpaths=[
-                            "//div[@class='ignore-container']/h3"
+                            "//div[@class='ignore-container']//div[@class='ignore-title']"
                         ],
-                        index_limit_count=1,
-                        css='.ignore-content-item{padding: 1rem 0;} .ignore-content-item-source{font-size:1.2rem;}',
+                        index_limit_count=2,
+                        css='.ignore-content-item{padding: 1rem 0;} .ignore-content-item-source{font-size: 1.2rem;}'
+                            '.ignore-content-item-title{font-weight: bold; color: rgb(233, 119, 43);}',
                         )
