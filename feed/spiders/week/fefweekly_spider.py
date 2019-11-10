1a058dcb086451ebbe1fd8931df0f249533a494a
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-11-10 22:06:04 +0800

    add site frontend focus

---
+++ feed/spiders/week/fefweekly_spider.py
@@ -0,0 +1,20 @@
+
+from feed.spiders.spider import Spider
+
+
+class FefWeeklySpider(Spider):
+    name = 'fefweekly'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://github.com/richshaw2015/frontendfocus-cn',
+                        ],
+                        index_xpath="//a[contains(text(),'第 ')]/@href",
+                        article_title_xpath='//article//h1/text()',
+                        article_content_xpath='//article',
+                        article_trim_xpaths=[
+                            '//h1'
+                        ],
+                        index_limit_count=1,
+                        )
