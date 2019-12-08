f163fadd59e4dc138017cc97f9ce8cd29707f98f
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-12-08 17:06:47 +0800

    fix ul li style

---
+++ feed/spiders/day/xingzx_spider.py
@@ -0,0 +1,20 @@
+
+from feed.spiders.spider import Spider
+
+
+class XingzxSpider(Spider):
+    name = 'xingzx'
+
+    def __init__(self):
+        Spider.__init__(self,
+                        start_urls=[
+                            'https://xingzx.org/',
+                        ],
+                        index_xpath="//h2[@class='post-title']/a/@href",
+                        article_title_xpath="//div[@class='markdown-body editormd-preview-container']/h1/text()",
+                        article_content_xpath="//div[@class='markdown-body editormd-preview-container']",
+                        index_limit_count=4,
+                        article_trim_xpaths=[
+                            "//div[@class='markdown-body editormd-preview-container']/h1"
+                        ]
+                        )
