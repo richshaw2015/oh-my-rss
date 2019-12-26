b6341461caf50b849c5a690ef0ed5d13b02f36ca
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-12-26 11:54:55 +0800

    fix: database is locked

--- ohmyrss/settings.py
+++ ohmyrss/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,  # in seconds
+        }
     }
 }
 
