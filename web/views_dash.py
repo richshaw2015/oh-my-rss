638e2f4164ee4816098c80205f0626e7b270d17a
xingzhongxiang <xingzhongxiang@youcheyihou.com>
2019-12-26 18:15:08 +0800

    fix: use local echart.js & optimize pie chart

--- web/views_dash.py
+++ web/views_dash.py
@@ -101,7 +101,7 @@ def refer_pie_chart() -> Pie:
         Pie()
         .add(
             "",
-            [list(z) for z in zip(refer_hosts, R.mget(*refer_host_pv_keys))],
+            [list(z) for z in zip(refer_hosts, R.mget(*refer_host_pv_keys)) if int(z[1]) > 20],
             # radius=["30%", "75%"],
             # rosetype="radius",
         )
