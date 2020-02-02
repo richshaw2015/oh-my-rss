982ebb5eeeb18dd195d12ef79d2d0d657e206317
Neo <richshaw@126.com>
2020-02-02 20:43:24 +0800

    fix sql True value

--- web/sql.py
+++ web/sql.py
@@ -3,7 +3,7 @@
 get_recent_articles_sql = '''
 SELECT *, COUNT("site_id") AS "new" 
 FROM "web_article" 
-WHERE ("web_article"."is_recent" = True AND "web_article"."status" = "active") 
+WHERE ("web_article"."is_recent" = 1 AND "web_article"."status" = "active") 
 GROUP BY "web_article"."site_id" 
 ORDER BY "web_article"."id" DESC 
 LIMIT 100;
