a8062b7b6dab1ca5fd7ff910912592f88c86fe28
Neo <richshaw@126.com>
2020-02-02 23:24:59 +0800

    limit explore contents

--- web/sql.py
+++ web/sql.py
@@ -1,9 +1,11 @@
 
 # 最近更新文章，按照站点分组
 get_recent_articles_sql = '''
-SELECT *, COUNT("site_id") AS "new" 
+SELECT "web_article"."id", COUNT("web_article"."site_id") AS "new"
 FROM "web_article" 
-WHERE ("web_article"."is_recent" = 1 AND "web_article"."status" = "active") 
+INNER JOIN "web_site"
+ON "web_article"."site_id" = "web_site"."id"
+WHERE ("web_article"."is_recent" = 1 AND "web_article"."status" = "active" AND "web_site"."star" >= 10) 
 GROUP BY "web_article"."site_id" 
 ORDER BY "web_article"."id" DESC 
 LIMIT 100;
--- web/views_html.py
+++ web/views_html.py
@@ -134,7 +134,7 @@ def get_recent_sites(request):
     if user:
         user_sub_feeds = get_user_sub_feeds(user.oauth_id)
 
-    sites = Site.objects.filter(status='active').order_by('-id')[:100]
+    sites = Site.objects.filter(status='active').order_by('-id')[:50]
     context = dict()
 
     context['user'] = user
