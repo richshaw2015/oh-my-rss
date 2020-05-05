799c38b34c4d2df543fecff08f8ca5744b152227
Neo <richshaw@126.com>
2020-05-06 00:24:48 +0800

    hide some site's update

--- web/sql.py
+++ web/sql.py
@@ -17,7 +17,8 @@ SELECT "web_article"."id", COUNT("web_article"."site_id") AS "new"
 FROM "web_article" 
 INNER JOIN "web_site"
 ON "web_article"."site_id" = "web_site"."id"
-WHERE ("web_article"."is_recent" = 1 AND "web_article"."status" = "active" AND "web_site"."star" < 10) 
+WHERE ("web_article"."is_recent" = 1 AND "web_article"."status" = "active" AND "web_site"."star" < 10 
+    AND "web_site"."star" > 2) 
 GROUP BY "web_article"."site_id" 
 ORDER BY "web_article"."id" DESC 
 LIMIT 100;
