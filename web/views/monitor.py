bf69d6ef9de1b89d59aae8edf21de9ac1ceab69c
Neo <richshaw@126.com>
2021-06-26 19:36:48 +0800

    process number

--- web/views/monitor.py
+++ web/views/monitor.py
@@ -23,7 +23,7 @@ def heart_beat(request):
     running_process = subprocess.getoutput("ps x")
 
     try:
-        assert 4 == running_process.count('ohmyrss.wsgi:application')
+        assert 2 == running_process.count('ohmyrss.wsgi:application')
     except:
         return HttpResponse("FAIL: ohmyrss.wsgi:application")
 
